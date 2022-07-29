ede4c049ac2b378a0b1d9b123a94950f4b8b1dba
Bhaswanth Ayapilla <bhaswanthayapilla@gmail.com>
2022-07-29 20:26:22 +0530

    change deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -20,7 +20,7 @@ _no_pages_branch=false
 
 _backup_dir="$(mktemp -d)"
 
-_baseurl=""
+_baseurl="/bhaswanth-a.github.io"
 
 help() {
   echo "Build, test and then deploy the site content to 'origin/<pages_branch>'"
